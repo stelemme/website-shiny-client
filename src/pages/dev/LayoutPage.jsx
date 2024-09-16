f13c6ca24270c2f699d6ca5c2fc07504b6d610c2
stefswebe <stef.lemmens@swecobelgium.be>
2024-09-16 11:01:10 +0200

    layout dev page

---
+++ src/pages/dev/LayoutPage.jsx
@@ -0,0 +1,21 @@
+// mui imports
+import { Grid } from "@mui/material";
+
+// Components imports
+import PageComponent from "../../components/General/PageComponent";
+
+export default function LayoutPage() {
+  return (
+    <>
+      <PageComponent
+        title="LAYOUTPAGE"
+        subtitle="Welcome to the dev layoutpage"
+      ></PageComponent>
+
+      <PageComponent
+        title="LAYOUTPAGE"
+        subtitle="Welcome to the dev layoutpage"
+      ></PageComponent>
+    </>
+  );
+}
