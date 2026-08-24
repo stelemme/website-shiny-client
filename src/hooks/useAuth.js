dee87b2cec3e1398f9cb39218f592c09528782d9
Stef Lemmens <stef-lemmens@hotmail.com>
2026-08-24 23:08:36 +0200

    username

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,13 +2,17 @@ import { useState, useEffect } from "react";
 
 // Firebase imports
 import { auth } from "../utils/firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, updateProfile } from "firebase/auth";
 
 export const useAuth = () => {
   const [login, setLogin] = useState(false)
   const [username, setUsername] = useState(undefined)
 
   useEffect(() => onAuthStateChanged(auth, (user) => {
+    // updateProfile(auth.currentUser, {
+    //   displayName: "Nathan"
+    // })
+
     if (user) {
       setLogin(true)
       setUsername(auth.currentUser.displayName)
